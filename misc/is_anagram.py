9837e034383b7f5b1dd68060980a832b4c5ec65f
Subbu Pendyala <veera.pendyala@fireeye.com>
2016-09-23 09:51:40 -0700

    Anagrams solution with primes.

--- misc/is_anagram.py
+++ misc/is_anagram.py
@@ -51,7 +51,37 @@ def anagram_sol2(str_1, str_2):  # 6 O(n) + O(26)
     return match_flag
 
 
-# TODO: 3rd solution using prime numbers.
+# 3rd solution using prime numbers. Assuming the set of Prime numbers
+# Not generating primes.
+# TODO: To generate primes.
+# As another utility and its Time and Space complexity
+
+# Assuming lower case ASCII Characters
+FIRST_26_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+                   59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
+
+def anagram_sol3(str_1, str_2):  # 6 O(n) + O(26)
+    # If string lenghts are not matching then it is invalid
+    # To find lenght of str_1 is O(n) if str_1 has n characters
+    # To find lenght of str_2 is O(m) if str_2 has m characters
+    if len(str_1) != len(str_2):
+        return False
+
+    product_val_of_str1 = 1
+    product_val_of_str2 = 1
+
+    for each_char in str_1:  # O(n)
+        index = ord(each_char) - ord('a')
+        product_val_of_str1 *= FIRST_26_PRIMES[index]
+
+    for each_char in str_2:  # O(n)
+        index = ord(each_char) - ord('a')
+        product_val_of_str2 *= FIRST_26_PRIMES[index]
+
+    if product_val_of_str1 != product_val_of_str2:
+        return False
+    return True
 
 
 assert anagram_sol1('abcd', 'dbca') == True
@@ -61,3 +91,7 @@ assert anagram_sol1('abcd', 'adca') == False
 assert anagram_sol2('abcd', 'dbca') == True
 assert anagram_sol2('abcd', 'adbca') == False
 assert anagram_sol2('abcd', 'adca') == False
+
+assert anagram_sol3('abcd', 'dbca') == True
+assert anagram_sol3('abcd', 'adbca') == False
+assert anagram_sol3('abcd', 'adca') == False
