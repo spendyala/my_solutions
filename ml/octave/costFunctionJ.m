a0c36e1f7e2face08c0a646df8006ece3f51d7bb
Veera Pendyala <vpendyala@C02SH46LG8WP.corp.proofpoint.com>
2017-11-22 11:14:35 -0800

    ML assingment 1

---
+++ ml/octave/costFunctionJ.m
@@ -0,0 +1,8 @@
+function J = costFunctionJ(X, y, theta)
+% X is the "design matrix" containing our tranining examples.
+% y is the class labels
+m = size(X, 1); % number of tranining examples
+predictions = X*theta; % predictions of hypothesis on all m examples
+sqrErrors = (predictions-y).^2; % squared errors
+
+J = 1/(2*m) * sum(sqrErrors);
---
+++ ml/octave/squareTheNumber.m
@@ -0,0 +1,2 @@
+function y = squareTheNumber(x)
+y = x^2;
